912ef4cd37d2a7b988557d47a1ef1e1ee2e5e5af
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-05 13:34:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import BillingDashboard from './components/BillingDashboard'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BillingDashboard />
+    </>
+  )
+}
+
+export default App
